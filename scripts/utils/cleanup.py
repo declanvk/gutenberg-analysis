6798154c0ccf044c98f81bb625b5b764c9913928
Declan Kelly <dkelly.home@gmail.com>
2018-03-06 20:23:42 -0800

    Add gutenberg utility module for acquiring and cleaning texts

---
+++ scripts/utils/cleanup.py
@@ -0,0 +1,64 @@
+"""Module to remove the noise from Project Gutenberg texts."""
+
+import os
+
+from text import TEXT_END_MARKERS, TEXT_START_MARKERS, LEGALESE_END_MARKERS, LEGALESE_START_MARKERS
+
+
+def strip_headers(text):
+    """Remove lines that are part of the Project Gutenberg header or footer.
+    Note: this function is a port of the C++ utility by Johannes Krugel. The
+    original version of the code can be found at:
+    http://www14.in.tum.de/spp1307/src/strip_headers.cpp
+
+    Args:
+        text (unicode): The body of the text to clean up.
+
+    Returns:
+        unicode: The text with any non-text content removed.
+
+    """
+    lines = text.splitlines()
+    sep = str(os.linesep)
+
+    out = []
+    i = 0
+    footer_found = False
+    ignore_section = False
+
+    for line in lines:
+        reset = False
+
+        if i <= 600:
+            # Check if the header ends here
+            if any(line.startswith(token) for token in TEXT_START_MARKERS):
+                reset = True
+
+            # If it's the end of the header, delete the output produced so far.
+            # May be done several times, if multiple lines occur indicating the
+            # end of the header
+            if reset:
+                out = []
+                continue
+
+        if i >= 100:
+            # Check if the footer begins here
+            if any(line.startswith(token) for token in TEXT_END_MARKERS):
+                footer_found = True
+
+            # If it's the beginning of the footer, stop output
+            if footer_found:
+                break
+
+        if any(line.startswith(token) for token in LEGALESE_START_MARKERS):
+            ignore_section = True
+            continue
+        elif any(line.startswith(token) for token in LEGALESE_END_MARKERS):
+            ignore_section = False
+            continue
+
+        if not ignore_section:
+            out.append(line.rstrip(sep))
+            i += 1
+
+    return sep.join(out)
